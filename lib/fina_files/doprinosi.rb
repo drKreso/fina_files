154a65ba180dc5c56b1b5e17a4f7005dae86133d
drKreso <kresimir.bojcic@gmail.com>
2013-01-03 23:38:22 +0100

    dijakriticki znakovi

--- lib/fina_files/doprinosi.rb
+++ lib/fina_files/doprinosi.rb
@@ -16,11 +16,11 @@ class Doprinosi
 
   def self.nalozi
    result = [
-    [ "HR68 8109-#{postavke[:oib_platitelja]}-#{godina_mjesec}",'MIO I STUP','1001005-1863000160','DRŽAVNI PRORAČUN REPUBLIKE HRVATSKE', postavke[:mio_i] ],
+    [ "HR68 8109-#{postavke[:oib_platitelja]}-#{godina_mjesec}",'MIO I STUP','1001005-1863000160','DRZAVNI PRORACUN REPUBLIKE HRVATSKE', postavke[:mio_i] ],
     [ "HR68 2003-#{postavke[:oib_platitelja]}-#{godina_mjesec}",'MIO II STUP','1001005-1700036001','DOPRINOS ZA MIROVINSKO OSIGURANJE', postavke[:mio_ii] ],
-    [ "HR68 8400-#{postavke[:oib_platitelja]}-#{godina_mjesec}",'Doprinos za osnovno zdravstveno osiguranja','1001005-1863000160','DRŽAVNI PRORAČUN REPUBLIKE HRVATSKE', postavke[:zdravstveno_osiguranje] ],
-    [ "HR68 8559-#{postavke[:oib_platitelja]}-#{godina_mjesec}",'Doprinos za ozljede na radu','1001005-1863000160','DRŽAVNI PRORAČUN REPUBLIKE HRVATSKE', postavke[:ozljede_na_radu] ],
-    [ "HR68 8702-#{postavke[:oib_platitelja]}-#{godina_mjesec}",'Doprinos za zaposljavanje','1001005-1863000160','DRŽAVNI PRORAČUN REPUBLIKE HRVATSKE', postavke[:zaposljavanje]],
+    [ "HR68 8400-#{postavke[:oib_platitelja]}-#{godina_mjesec}",'Doprinos za osnovno zdravstveno osiguranja','1001005-1863000160','DRZAVNI PRORACUN REPUBLIKE HRVATSKE', postavke[:zdravstveno_osiguranje] ],
+    [ "HR68 8559-#{postavke[:oib_platitelja]}-#{godina_mjesec}",'Doprinos za ozljede na radu','1001005-1863000160','DRZAVNI PRORACUN REPUBLIKE HRVATSKE', postavke[:ozljede_na_radu] ],
+    [ "HR68 8702-#{postavke[:oib_platitelja]}-#{godina_mjesec}",'Doprinos za zaposljavanje','1001005-1863000160','DRZAVNI PRORACUN REPUBLIKE HRVATSKE', postavke[:zaposljavanje]],
    ]
     #broj racuna je za grad Zagreb
     unless postavke[:porez_prirez_dohodak].nil?
@@ -33,7 +33,7 @@ class Doprinosi
       end
     end
     unless postavke[:pdv].nil?
-      result << [ "HR68 1201-#{postavke[:oib_platitelja]}","PDV #{godina_mjesec}",'1001005-1863000160','DRŽAVNI PRORAČUN REPUBLIKE HRVATSKE', postavke[:pdv]]
+      result << [ "HR68 1201-#{postavke[:oib_platitelja]}","PDV #{godina_mjesec}",'1001005-1863000160','DRZAVNI PRORACUN REPUBLIKE HRVATSKE', postavke[:pdv]]
     end
     result
  end
